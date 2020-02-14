733ec4952857eaa98f72345627c7ff59ebc1eefe
Ifeanyi Igbo <1feanyi@users.noreply.github.com>
2020-02-14 13:17:00 -0500

    Add error handling

--- MatingApp-SPA/src/app/register/register.component.ts
+++ MatingApp-SPA/src/app/register/register.component.ts
@@ -17,7 +17,7 @@ export class RegisterComponent implements OnInit {
 
   register() {
     this.authService.register(this.model).subscribe(() => {
-      console.log('Registration successfu');
+      console.log('Registration successful');
     }, error => {
       console.log(error);
     });
